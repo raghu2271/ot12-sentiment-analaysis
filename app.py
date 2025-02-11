ec6fc1654ac233ad6facf4d3eed64ef56d1ee392
Madhura73 <gajammadhura73@gmail.com>
2025-02-11 09:32:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,23 @@ def Home():
 @app.route("/predict", methods=["GET", "POST"])
 def predict():
     if request.method == "POST":
+        msg =request.form.get("message")
+        print(msg)
+        c1=TextToNum(msg)
+        c1.cleaner()
+        c1.token
+        c1.removeStop()
+        st=c1.stemme()
+        stvc=" ".join(st);
+        with open("vectorizer.pickle","rb") as vc_file:
+            vectorizer=pickle.load(vc_file)
+        dt=vectorizer.transform([stvc]).toarray()
+        with open ("model.pickle","rb") as mb_file:
+            model=pickle.load(mb_file)
+        pred=model.predict(dt)
+        print(pred)
+        return jsonify({"prediction":pred})
+    if request.method == "POST":
         msg = request.form.get("message")  # Get the message from the form
         print(msg)  # You can add logic to process the message here
         return render_template("predict.html", msg=msg)  # Pass the message to the template if needed
